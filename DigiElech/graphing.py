f1aed38d535a37d1fec69b9f129b920694d19a06
lezra1 <130955254+lezra1@users.noreply.github.com>
2023-05-25 15:04:46 +1000

    rename and adding more simulations

---
+++ DigiElech/graphing.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 10:11:17 2023
+
+@author: Nathan
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+  
+voltage = []
+current = []
+  
+f = open('CV_kf_100_kb_2.0416.txt','r')
+f = open('CV_kf_1e7_kb_1.5821e-27.txt','r')
+for row in f:
+    row = row.split()
+    if len(row) > 2:
+        continue
+    else:
+        voltage.append(float(row[0]))
+        current.append(float(row[1]))
+  
+#correction for DigiElch's current being flipped
+current = np.array(current) * (-1)
+
+plt.plot(voltage, current, color = 'g', label = 'File Data')
+  
+plt.cla()
+plt.plot(voltage, current)
+plt.title("'CV_kf_100_kb_2.0416.txt','r'")
+plt.xlabel("Eapp [V]")
+plt.ylabel("current [A]")
